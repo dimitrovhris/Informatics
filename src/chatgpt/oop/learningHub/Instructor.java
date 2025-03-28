ac1c530a67df64cfbeb4823f2dd6dfdd83601240
Hristiyan Dimitrov <hrisit12@gmail.com>
2025-03-28 09:17:09 +0200

    Create Instructor.java
    Informatics Exam

---
+++ src/chatgpt/oop/learningHub/Instructor.java
@@ -0,0 +1,11 @@
+public class Instructor {
+    private int experienceYears;
+    private String speciality;
+
+    public Instructor(int experienceYears, String speciality) {
+        this.experienceYears = experienceYears;
+        this.speciality = speciality;
+    }
+
+    
+}
