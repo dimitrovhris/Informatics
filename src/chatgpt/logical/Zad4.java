783e3642b826b0f6b7fd0242869e6a0167d1664b
Hristiyan Dimitrov <hrisit12@gmail.com>
2025-04-15 13:03:49 +0300

    Create Zad4.java
    Training for Informatics Exam

---
+++ src/chatgpt/logical/Zad4.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Zad26_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        int[] heights = new int[n];
+        String[] inputHeights = sc.nextLine().split(" ");
+
+        for(int i = 0; i < heights.length ; i++){
+            heights[i] = Integer.parseInt(inputHeights[i]);
+        }
+
+        int violations = 0;
+        for(int i = 0; i < heights.length - 1; i++){
+            if(heights[i] > heights[i+1]){
+                violations++;
+            }
+        }
+        int swaps = 0;
+
+        for(int i = 0; i < heights.length - 1; i++){
+            for(int j = 0; j < heights.length - 1 - i; j++){
+                if(heights[j] > heights[j+1]){
+                    int temp = heights[j];
+                    heights[j] = heights[j + 1];
+                    heights[j+1] = temp;
+                    swaps += 1;
+                }
+            }
+        }
+        System.out.println(violations);
+        System.out.println(swaps);
+    }
+}
